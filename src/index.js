fd40bb6c86add8caa7d4b21aa43a21c08171b32e
Alexander <urevaleksandr468@gmail.com>
2024-04-23 21:59:22 +0300

    new

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
+    <BrowserRouter basename="/">
     <App />
+    </BrowserRouter>
   </Provider>
 );
